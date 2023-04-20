a06037c6962481df8234237c4b2f89b45f59fde6
Martin Fraga <martin.fraga@lambdaclass.com>
2023-04-20 09:07:17 -0300

    idk what I did but now it's not having collisions

--- practica_1/dimmer_tb.vhdl
+++ practica_1/dimmer_tb.vhdl
@@ -11,6 +11,7 @@ ARCHITECTURE behavior OF dimmer_tb IS
     CONSTANT N : NATURAL := 3;
     --Inputs
     SIGNAL clk_i : STD_LOGIC := '1';
+    SIGNAL reset_i : STD_LOGIC := '1';
     SIGNAL duty_cycle_i : STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
     SIGNAL out_o : STD_LOGIC;
     -- Clock period definitions
@@ -26,6 +27,7 @@ BEGIN
     END PROCESS;
 
     duty_cycle_i <= "000", "111" AFTER 15 us, "011" after 30 us, "000" after 45 us;
+    reset_i <= '1', '0' AFTER 5 us;
 
     -- Instantiate the Unit Under Test (UUT)
     uut : entity work.dimmer
@@ -34,6 +36,7 @@ BEGIN
     )
     PORT MAP(
         clk_i => clk_i,
+        reset_i => reset_i,
         duty_cycle_i => duty_cycle_i,
         out_o => out_o
     );
