3aaaac9d5f93e288a3a6941515abbf82eca30c33
Nikita Zdanovitch <nzdanovitch@gmail.com>
2023-05-12 01:21:54 -0300

    Add project scaffold script.

---
+++ new_project.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+set -ex
+PROJECT_NAME="$1" # FIRST ARG
+
+rm -fr __project_template__/*
+git checkout __project_template__
+rm -fr "$PROJECT_NAME"
+cp -r __project_template__ "$PROJECT_NAME"
+
+for f in ./$PROJECT_NAME/__project_template__.*; do
+    mv "$f" "$(echo "$f" | sed s/__project_template__/$PROJECT_NAME/)";
+done
+
+for f in ./$PROJECT_NAME/*; do
+    sed -i "s/__project_template__/$PROJECT_NAME/g" "$f"
+done
+
+
