d58cd0eb091588f773487257d2942c6fb2d95eff
Nikita Zdanovitch <nzdanovitch@gmail.com>
2023-05-14 16:30:12 -0300

    Assemble sof with quartus.

--- __project_template__/gen_svf.py
+++ __project_template__/gen_svf.py
@@ -2,15 +2,23 @@ import sys
 import os
 import subprocess
 
+
+project_path = sys.path[0]
+project_name = os.path.basename(project_path)
+
+
 #
 # Generate .sof file
 #
 
+subprocess.run(["quartus_map", project_name], check=True)
+subprocess.run(["quartus_fit", project_name], check=True)
+subprocess.run(["quartus_asm", project_name], check=True)
+
+
 #
 # Generate .cdf file
 #
-project_path = sys.path[0]
-project_name = os.path.basename(project_path)
 
 cdf_contents = f"""/* Quartus II 64-Bit Version 13.0.1 Build 232 06/12/2013 Service Pack 1 SJ Web Edition */
 JedecChain;
@@ -18,7 +26,7 @@ JedecChain;
   DefaultMfr(6E);
 
   P ActionCode(Cfg)
-    Device PartName(EP2C5T144) Path("{project_path}") File("{project_name}.sof") MfrSpec(OpMask(1));
+    Device PartName(EP2C5T144) Path("{project_path}/") File("{project_name}.sof") MfrSpec(OpMask(1));
 
 ChainEnd;
 
@@ -35,7 +43,13 @@ with open(cdf_path, 'w') as ofile:
 #
 # Generate .svf file
 #
-subprocess.run(['quartus_cpf', '-c', cdf_path, 'svf'], checked=True)
+subprocess.run([
+    'quartus_cpf',
+    '-c',
+    '-q', '25MHz', '-g', '3.3', '-n', 'p',
+    cdf_path,
+    f'{project_name}.svf'
+    ], check=True)
 
 #
 # Cleanup
