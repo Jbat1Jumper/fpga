5b14082d48a91678f94bfbaddba5dadfc9fe49b7
Martin Fraga <martin.fraga@lambdaclass.com>
2023-07-11 16:05:25 -0300

    add sample midi file

--- proyecto_final/midi2uart/from_midi_file.py
+++ proyecto_final/midi2uart/from_midi_file.py
@@ -1,5 +1,11 @@
+import mido
 import serial
 import platform
+from mido import MidiFile
+import time
+
+if platform.system() == "Darwin":
+    mido.set_backend("mido.backends.pygame")
 
 # TODO check OS
 ARDUINO_PATH = "/dev/tty.usbserial-130" if platform.system() == "Darwin" else "/dev/ttyUSB0"
@@ -7,9 +13,13 @@ ARDUINO_PATH = "/dev/tty.usbserial-130" if platform.system() == "Darwin" else "/
 print("Opening serial port")
 arduino = serial.Serial(port=ARDUINO_PATH, baudrate=115200, timeout=.5)
 
-print("Listening to notes")
-with open("CMIINTRO.MID", 'rb') as infile:
-    byte = infile.read(1)
-    while byte != b"":
-        print('written_bytes:', arduino.write(byte))
-        byte = infile.read()
+values = [0, 1, 2, 3]
+
+print("Playing Song")
+for msg in MidiFile('CMIIntro.MID'):
+    time.sleep(msg.time)
+    if not msg.is_meta:
+        msg_bytes_h = ','.join('{:02x}'.format(x) for x in msg.bytes()).upper()
+        biites = (msg.note % 4).to_bytes(length=1, byteorder="little")
+        print(msg, 'bytes:', msg_bytes_h, 'sending:', biites)
+        print('written_bytes:', arduino.write(biites))
